6e7d354dffa0372dfa9b0c11260533a0c1aa6927
tuvtju <pennylanett@gmail.com>
2022-04-07 10:02:01 +0200

    try

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,6 +1,10 @@
 package src.main;
 
+import java.awt.Graphics;
+
 import src.main.View.BunnyView;
+
+import src.main.entity.Player;
 
 public class Game implements Runnable{
 
@@ -8,14 +12,21 @@ public class Game implements Runnable{
     private BunnyView view;
     private Thread thread;
     private final int FPS = 120;
+    private final int UPS = 200;
+    private Player player;
 
     public Game() {
-
-       view = new BunnyView();
-       frame = new Window(view);
-       view.requestFocus();
-       Loop();
+        initClass();
+        view = new BunnyView(this);
+        frame = new Window(view);
+        view.requestFocus();
        
+        Loop();
+       
+    }
+
+    private void initClass() {
+        player = new Player(0,0);
     }
 
     private void Loop(){
@@ -23,29 +34,57 @@ public class Game implements Runnable{
         thread.start();
     }
 
+    public void update(){
+        player.update();
+    }
+
+    public void render(Graphics g){
+        player.render(g);
+
+    }
+
     @Override
     public void run() {
 
         double timePerFrame = 1000000000.0/FPS;
-        long lastFrame = System.nanoTime();
-        long now = System.nanoTime();
+        double timePerUpdate = 1000000000.0/UPS;
+        long previousTime = System.nanoTime();
         int frames = 0;
+        int update = 0;
         long lastcheck = System.currentTimeMillis();
+
+        double deltaU = 0;
+        double deltaF = 0;
         while(true){
 
-            now = System.nanoTime();
-            if(now - lastFrame >= timePerFrame){
+            long currentTime = System.nanoTime();
+
+            deltaU += (currentTime - previousTime)/ timePerUpdate;
+            deltaF += (currentTime - previousTime) / timePerFrame;
+            previousTime = currentTime;
+            if(deltaU >= 1){
+                update++;
+                deltaU--;
+            }
+
+            if(deltaF >= 1){
                 view.repaint();
-                lastFrame = now;
+                deltaF--;
                 frames++;
             }
+
             if(System.currentTimeMillis() -lastcheck >= 1000){
                 lastcheck = System.currentTimeMillis();
-                System.out.println("FPS: " + frames);
+                System.out.println("FPS: " + frames + "  | UPS: " + update);
                 frames = 0;
+                update = 0;
             }
         }
         
+    }
+
+    public Player getPlayer(){
+        return player;
     }
     
 }
